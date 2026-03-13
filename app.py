0817ca94a8937092de321e522e1986fb20f3528d
MOHMMAD FAROOQ BHAT <266348170+FBIITISM@users.noreply.github.com>
2026-03-13 07:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,3 @@ def create_order():
     orders.append(new_order)
     return jsonify({'success': True, 'order_id': new_order['id']}), 201
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
